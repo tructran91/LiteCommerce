d4036897464e432a9b86af16530e41158f4d60f9
Truc <trutran@strongtie.com>
2026-06-03 22:04:35 +0700

    add product template page

---
+++ Web/LiteCommerce.Admin/Models/Business/ProductTemplate/ProductTemplateQuery.cs
@@ -0,0 +1,11 @@
+﻿using LiteCommerce.Admin.Constants;
+
+namespace LiteCommerce.Admin.Models.Business.ProductTemplate
+{
+    public class ProductTemplateQuery
+    {
+        public int Page { get; set; } = 1;
+
+        public int PageSize { get; set; } = AppConstants.PageSize;
+    }
+}
---
+++ Web/LiteCommerce.Admin/Pages/Catalog/ProductTemplates/ProductTemplateManagement.razor.cs
@@ -0,0 +1,175 @@
+﻿using LiteCommerce.Admin.ApiClients;
+using LiteCommerce.Admin.Constants;
+using LiteCommerce.Admin.Models.Business.ProductAttribute;
+using LiteCommerce.Admin.Models.Business.ProductTemplate;
+using LiteCommerce.Admin.Models.Common;
+using LiteCommerce.Admin.Pages.Base;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace LiteCommerce.Admin.Pages.Catalog.ProductTemplates
+{
+    public partial class ProductTemplateManagement
+    {
+        [Inject]
+        private IProductTemplateApi ProductTemplateApi { get; set; }
+
+        [Inject]
+        private IProductAttributeApi ProductAttributeApi { get; set; }
+
+        [Inject]
+        private IDialogService DialogService { get; set; }
+
+        [Inject]
+        private ISnackbar Snackbar { get; set; }
+
+        private DeleteOperationHelper _deleteHelper => new(DialogService, Snackbar);
+
+        private List<BreadcrumbItem> _breadcrumbs = new()
+        {
+            new("Home", href: "/", icon: Icons.Material.Filled.Home),
+            new("Catalog", href: null, disabled: true),
+            new("Product Templates", href: null, disabled: true),
+        };
+
+        private MudTable<ProductTemplateResponse> _table = null!;
+        private BaseResponse<List<ProductTemplateResponse>>? _pagedResult;
+        private List<ProductAttributeResponse> _productAttributes = new();
+        private bool _loading = false;
+        private string? _errorMessage;
+        private ProductTemplateQuery _query = new();
+
+        private DialogOptions _dialogOptions = new()
+        {
+            MaxWidth = MaxWidth.Medium,
+            FullWidth = true,
+            CloseOnEscapeKey = true,
+            BackdropClick = false,
+        };
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            await LoadProductAttributes();
+        }
+
+        private async Task LoadProductAttributes()
+        {
+            var response = await ProductAttributeApi.GetProductAttributesAsync(1, 100);
+            if (response.IsSuccess)
+            {
+                _productAttributes = response.Data;
+            }
+        }
+
+        private async Task<TableData<ProductTemplateResponse>> ServerReload(TableState state, CancellationToken ct)
+        {
+            await LoadData();
+
+            return new TableData<ProductTemplateResponse>
+            {
+                Items = _pagedResult?.Data ?? new(),
+                TotalItems = _pagedResult?.Pagination.TotalRecords ?? 0,
+            };
+        }
+
+        private async Task LoadData()
+        {
+            _loading = true;
+            _errorMessage = null;
+
+            var result = await ProductTemplateApi.GetProductTemplatesAsync(_query.Page, _query.PageSize);
+            if (result.IsSuccess)
+            {
+                _pagedResult = result;
+            }
+            else
+            {
+                _errorMessage = result.Message;
+                Snackbar.Add(result.Message ?? SystemMessages.ErrorOccurred, Severity.Error);
+            }
+
+            _loading = false;
+            StateHasChanged();
+        }
+
+        private async Task ReloadTable() => await _table.ReloadServerData();
+
+        private async Task OnPageChanged(int page)
+        {
+            _query.Page = page;
+            await ReloadTable();
+        }
+
+        private async Task OnPageSizeChanged(int size)
+        {
+            _query.PageSize = size;
+            _query.Page = 1;
+            await ReloadTable();
+        }
+
+        private async Task OpenTemplateDialog(bool isEdit, ProductTemplateResponse? productTemplate = null)
+        {
+            var parameters = new DialogParameters
+            {
+                [nameof(ProductTemplateDialog.IsEdit)] = isEdit,
+                [nameof(ProductTemplateDialog.InitialId)] = isEdit ? productTemplate?.Id : null,
+                [nameof(ProductTemplateDialog.InitialName)] = isEdit ? productTemplate?.Name ?? "" : "",
+                [nameof(ProductTemplateDialog.AllAttributes)] = _productAttributes,
+                [nameof(ProductTemplateDialog.InitialSelectedAttributes)] = isEdit ? productTemplate?.ProductAttributes : null,
+            };
+
+            var dialog = await DialogService.ShowAsync<ProductTemplateDialog>("", parameters, _dialogOptions);
+            var result = await dialog.Result;
+
+            if (result is null || result.Canceled || result.Data is not ProductTemplateDialog.TemplateDialogResult data)
+                return;
+
+            _loading = true;
+
+            var templateForm = new ProductTemplateFormModel
+            {
+                Id = data.IsEdit ? data.Id : null,
+                Name = data.Name,
+                ProductAttributes = data.ProductAttributes
+            };
+
+            var response = data.IsEdit
+                ? await ProductTemplateApi.UpdateProductTemplateAsync(templateForm)
+                : await ProductTemplateApi.CreateProductTemplateAsync(templateForm);
+
+            if (response.IsSuccess)
+            {
+                var successMessage = data.IsEdit
+                    ? SystemMessages.UpdateDataSuccess
+                    : SystemMessages.AddDataSuccess;
+
+                Snackbar.Add(successMessage, Severity.Success);
+            }
+            else
+            {
+                var msg = response.Message ?? SystemMessages.ErrorOccurred;
+                Snackbar.Add(msg, Severity.Error);
+            }
+
+            await ReloadTable();
+            _loading = false;
+            StateHasChanged();
+        }
+
+        private async Task OpenDeleteConfirm(ProductTemplateResponse productTemplate)
+        {
+            _loading = true;
+
+            await _deleteHelper.ExecuteDeleteOperation(
+                productTemplate.Id,
+                productTemplate.Name,
+                ProductTemplateApi.DeleteProductTemplateAsync,
+                ReloadTable
+            );
+
+            _loading = false;
+            StateHasChanged();
+        }
+    }
+}
